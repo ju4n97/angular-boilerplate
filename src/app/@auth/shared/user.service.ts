b5b69efd8e66bfb742316f699e2b4c6fb0438f23
juanmesa2097 <juanmesa2097@gmail.com>
2020-06-08 16:59:50 -0500

    ✨ Create user's service

---
+++ src/app/@auth/shared/user.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { GenericHttpService } from '@app/@core/services';
+import { environment } from '@environments/environment';
+import { UserAdapter } from './user.adapter';
+import { User } from './user.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService extends GenericHttpService<User> {
+  constructor(httpClient: HttpClient) {
+    super(httpClient, environment.apiUrl, 'users', new UserAdapter());
+  }
+}
