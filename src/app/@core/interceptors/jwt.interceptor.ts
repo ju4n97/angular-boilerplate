832b63f22efa18fe2d0971b4a4317797ab8409b1
juanmesa2097 <juanmesa2097@gmail.com>
2020-06-08 22:12:44 -0500

    ✨ Create JWT interceptor

---
+++ src/app/@core/interceptors/jwt.interceptor.ts
@@ -0,0 +1,34 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthService } from '@app/@auth';
+import { environment } from '@environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const currentUser = this.authService.currentUserValue;
+    const isLoggedIn = currentUser && currentUser.token;
+    const isApiUrl = request.url.startsWith(environment.apiUrl);
+
+    if (isLoggedIn && isApiUrl) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${currentUser.token}`,
+        },
+      });
+    }
+
+    return next.handle(request);
+  }
+}
