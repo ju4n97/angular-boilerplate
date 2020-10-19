97c6ca0555a1743956a1a1348cbb1cec907eb59e
juanmesa2097 <juanmesa2097@gmail.com>
2020-10-19 18:33:48 -0500

    chore(): declare public components

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { HomePage } from './pages/home/home.page';
+import { InternalServerErrorPage } from './pages/internal-server-error/internal-server-error.page';
+import { NotFoundPage } from './pages/not-found/not-found.page';
 import { PublicRoutingModule } from './public-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [HomePage, NotFoundPage, InternalServerErrorPage],
   imports: [CommonModule, PublicRoutingModule],
 })
 export class PublicModule {}
