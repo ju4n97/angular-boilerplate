34c210db1e61352c5554837dbeb8fb3a9bcb2ec4
juanmesa2097 <juanmesa2097@gmail.com>
2020-11-15 16:25:48 -0500

    chore: add sign in route

--- src/app/+auth/pages/sign-in/sign-in.page.route.ts
+++ src/app/+auth/pages/sign-in/sign-in.page.route.ts
@@ -6,7 +6,7 @@ export const SIGN_IN_ROUTE: Route = {
   path: Path.SignIn,
   component: SignInPage,
   data: {
-    title: 'Sign in on Angular Boilerplate',
+    title: 'Sign into Angular Boilerplate',
     description:
       'Start writing your business logic without any concern on architecture matters.',
     robots: 'index, follow',
