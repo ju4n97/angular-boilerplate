667615d4fa6a7f1e73c2e1d70870d73d47dc797b
juanmesa2097 <juanmesa2097@gmail.com>
2022-06-12 14:10:28 -0500

    Refactor home content

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,16 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { ThemeList, ThemeService } from '@lib/services/theme';
-import { ROUTER_UTILS } from '@lib/utils/router.utils';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.css'],
 })
 export class HomePage {
-  path = ROUTER_UTILS.config;
   theme = ThemeList;
 
   constructor(private themeService: ThemeService) {}
