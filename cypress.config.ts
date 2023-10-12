3fcf785ede4cb1c114e65adce0f7dee6983baeb0
Dario Presutti <dariopresde@protonmail.com>
2023-10-12 19:33:47 +0200

    add baseUrl

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        baseUrl: 'http://localhost:4200',
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
     },
-  },
-  component: {
-    devServer: {
-      framework: 'angular',
-      bundler: 'webpack',
+    component: {
+        devServer: {
+            framework: 'angular',
+            bundler: 'webpack',
+        },
+        specPattern: '**/*.cy.ts',
     },
-    specPattern: '**/*.cy.ts',
-  },
 });
