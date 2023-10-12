62b44b2d48082fe5009302723037a538412210d5
Dario Presutti <dariopresde@protonmail.com>
2023-10-12 19:32:32 +0200

    add auth flow text

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,27 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('auth flow', () => {
+    it('Succesfully redirect if no logged in', () => {
+        cy.visit('/');
+        cy.location('pathname').should('eq', '/auth/login');
+    });
+
+    it('Succesfully redirect to home at log in', () => {
+        cy.visit('/auth/login');
+        cy.get('#login-button').click();
+        cy.location('pathname').should('eq', '/');
+    });
+
+    it('succesfully route to home if user is present in storage', () => {
+        window.localStorage.setItem('appSession', '{"user":"some-user-id","token":"abc"}');
+        cy.visit('/');
+        cy.location('pathname').should('eq', '/');
+    });
+});
+
+describe('home spec', () => {
+    it('Succesfully redirect if no logged in', () => {
+        cy.login();
+        cy.location('pathname').should('eq', '/');
+    });
+
+});
+
