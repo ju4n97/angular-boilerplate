11da56edfed22770d5572112d2c4c8fa4621a831
Dario Presutti <dariopresde@protonmail.com>
2023-10-16 19:20:45 +0200

    move declaration on .d.ts file
    I found this solution in this issue https://github.com/cypress-io/cypress-documentation/issues/4043O

---
+++ cypress/support/command.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+
+declare namespace Cypress {
+  interface Chainable {
+    /**
+     * Custom command to type a few random words into input elements
+     * @param count=3
+     * @example cy.get('input').typeRandomWords()
+     */
+    login(): Chainable<AUTWindow>;
+  }
+}
+
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -13,16 +13,3 @@ Cypress.Commands.add('login', () => {
     window.localStorage.setItem('appSession', '{"user":"some-user-id","token":"abc"}');
     cy.visit('/');
 });
-
-declare global {
-    namespace Cypress {
-        interface Chainable {
-            /**
-             * Custom command to type a few random words into input elements
-             * @param count=3
-             * @example cy.get('input').typeRandomWords()
-             */
-            login(): Chainable<any>;
-        }
-    }
-}
