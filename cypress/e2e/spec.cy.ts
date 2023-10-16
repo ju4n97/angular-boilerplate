3e2312fe6e4c86acb00cb29f270584ecafd3d7de
Dario Presutti <dariopresde@protonmail.com>
2023-10-16 18:54:30 +0200

    add switch theme test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -23,5 +23,19 @@ describe('home spec', () => {
         cy.location('pathname').should('eq', '/');
     });
 
+    it('Succesfully switch theme to dark', () => {
+        cy.login();
+        const darkThemeBtn = cy.get('#theme-dark-button');
+        darkThemeBtn.click();
+        cy.get('body').should('have.class', 'dark')
+    });
+
+    it('Succesfully switch theme to light', () => {
+        cy.login();
+        const darkThemeBtn = cy.get('#theme-light-button');
+        darkThemeBtn.click();
+        cy.get('body').should('have.class', 'light')
+    });
+
 });
 
