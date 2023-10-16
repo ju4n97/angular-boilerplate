8f8248c1b7e54216468abdbc662c4a64178cd7d9
Dario Presutti <dariopresde@protonmail.com>
2023-10-16 20:12:48 +0200

    add component configuration

---
+++ cypress/support/component.ts
@@ -0,0 +1,10 @@
+// Import commands.js using ES2015 syntax:
+import './commands'
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
+
+import { mount } from 'cypress/angular'
+
+
+Cypress.Commands.add('mount', mount)
